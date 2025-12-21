a55043ebdcf0f41c105d65bd5074ac8ed1b872d3
Abhishek Kumar <142258444+abhinf104@users.noreply.github.com>
2025-12-21 17:08:47 +0530

    Need the flask file from other file

--- Drawing2CAD/model/model.py
+++ Drawing2CAD/model/model.py
@@ -199,7 +199,7 @@ class SVG2CADTransformer(nn.Module):
         
         """command-guided generation"""
         # We need to call a single decoder here and maintain the input output format 
-        import pdb; pdb.set_trace()
+        # import pdb; pdb.set_trace()
         command_logits, guidance = self.command_decoder(z)
         command_logits = _make_batch_first(command_logits)
         args_logits = self.args_decoder(z, guidance)
