10d6d03abf9810c215ef36153bc3f9cc95850bca
Abhishek Kumar <142258444+abhinf104@users.noreply.github.com>
2025-12-21 16:38:24 +0530

    Added folder

--- DeepCAD/utils/export2step.py
+++ DeepCAD/utils/export2step.py
@@ -35,7 +35,7 @@ for path in out_paths:
     try:
         if args.form == "h5":
             with h5py.File(path, 'r') as fp:
-                out_vec = fp["out_vec"][:].astype(np.float)
+                out_vec = fp["vec"][:].astype(np.float)
                 out_shape = vec2CADsolid(out_vec)
         else:
             with open(path, 'r') as fp:
@@ -45,7 +45,7 @@ for path in out_paths:
             out_shape = create_CAD(cad_seq)
 
     except Exception as e:
-        print("load and create failed.")
+        print(f"load and create failed: {e}")
         continue
     
     if args.filter:
